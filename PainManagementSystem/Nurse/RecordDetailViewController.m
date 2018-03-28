4e4c5469d58b49a3057166a4056f4eaa1705322d
simplettton <515315183@qq.com>
2018-03-28 18:06:34 +0800

    fack data

--- PainManagementSystem/Nurse/RecordDetailViewController.m
+++ PainManagementSystem/Nurse/RecordDetailViewController.m
@@ -25,6 +25,9 @@
     NSArray *westArray;
     NSArray *eastArray;
 }
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:YES];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -37,7 +40,6 @@
     
     //西方诊断类别
     westArray = [NSArray arrayWithArray:dataDict[@"questionw"]];
-    
     
     NSMutableArray *westTypeNames = [[NSMutableArray alloc]initWithCapacity:20];;
     
@@ -80,7 +82,9 @@
     if (tableView == self.rootTableView) {
         return [titles count];;
     }else if(tableView.tag == KWestTableViewTag){
+        NSLog(@"weatArray count= %lu",(unsigned long)[westArray count]);
         return [westArray count];
+
     }else{
         return [eastArray count];
     }
@@ -91,58 +95,80 @@
     return 1;
 }
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    //基本情况
-    if (indexPath.section == [titles indexOfObject:@"基本情况"]) {
-        return 4*KRowInterval+3*KRowHeight+KTitleViewHeight+KPartInterval;
+    if (tableView == self.rootTableView) {
+        //基本情况
+        if (indexPath.section == [titles indexOfObject:@"基本情况"]) {
+            return 4*KRowInterval+3*KRowHeight+KTitleViewHeight+KPartInterval;
+        }
+        //诊断结果
+        if ((indexPath.section == [titles indexOfObject:@"诊断结果"]) || (indexPath.section == [titles indexOfObject:@"物理治疗方法"])) {
+            return 2*KRowInterval+1*KRowHeight+KTitleViewHeight+KPartInterval;
+        }
+        return 350;
+    }else{
+        return 44;
     }
-    //诊断结果
-    if ((indexPath.section == [titles indexOfObject:@"诊断结果"]) || (indexPath.section == [titles indexOfObject:@"物理治疗方法"])) {
-        return 2*KRowInterval+1*KRowHeight+KTitleViewHeight+KPartInterval;
-    }
-    return 350;
+
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     static NSString *CellIdentifier;
-    switch (indexPath.section) {
+    if (tableView == self.rootTableView) {
+        switch (indexPath.section) {
+                
+            case 0: {   CellIdentifier = @"BasicInfomationCell";    }       break;
+            case 3: {   CellIdentifier = @"ResultCell";     }
+            case 4: {   CellIdentifier = @"ResultCell";     }       break;
+            default: {      CellIdentifier = @"Cell";       }       break;
+        }
+        
+        RecordItemCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+        if (cell == nil) {
+            cell = [[RecordItemCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        }
+        cell.titleLabel.text = titles[indexPath.section];
+        
+        switch (indexPath.section) {
+            case 1:
+            {
+                cell.insertTableView.tag = KWestTableViewTag;
+            }
+                break;
+            case 2:
+            {
+                cell.insertTableView.tag = KEastTableViewTag;
+            }
+                break;
+            case 3:
+            {   UILabel *content = [cell viewWithTag:KContentTag];
+                content.text = [NSString stringWithFormat:@"病理因素：痉挛性疼痛          发病部位：肌肉          中医辨证：寒"];
+            }
+                break;
+            case 4:
+            {
+                UILabel *content = [cell viewWithTag:KContentTag];
+                content.text = [NSString stringWithFormat:@"超声治疗法"];
+            }
+                break;
+            default: {      CellIdentifier = @"Cell";       }       break;
+        }
+           return cell;
+    }else {
+        
+        CellIdentifier = @"QuestionCell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        }
+        if(tableView.tag == KWestTableViewTag){
             
-        case 0: {   CellIdentifier = @"BasicInfomationCell";    }       break;
-        case 3: {   CellIdentifier = @"ResultCell";     }
-        case 4: {   CellIdentifier = @"ResultCell";     }       break;
-        default: {      CellIdentifier = @"Cell";       }       break;
+        }else{
+            
+        }
+        return cell;
+        
     }
-    
-    RecordItemCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    if (cell == nil) {
-        cell = [[RecordItemCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-    }
-    cell.titleLabel.text = titles[indexPath.section];
-    
-    switch (indexPath.section) {
-        case 1:
-        {
-            cell.insertTableView.tag = KWestTableViewTag;
-        }
-            break;
-        case 2:
-        {
-            cell.insertTableView.tag = KEastTableViewTag;
-        }
-            break;
-        case 3:
-        {   UILabel *content = [cell viewWithTag:KContentTag];
-            content.text = [NSString stringWithFormat:@"病理因素：痉挛性疼痛          发病部位：肌肉          中医辨证：寒"];
-        }
-            break;
-        case 4:
-        {
-            UILabel *content = [cell viewWithTag:KContentTag];
-            content.text = [NSString stringWithFormat:@"超声治疗法"];
-        }
-            break;
-        default: {      CellIdentifier = @"Cell";       }       break;
-    }
-    
-    return cell;
+
+
 }
 
 
